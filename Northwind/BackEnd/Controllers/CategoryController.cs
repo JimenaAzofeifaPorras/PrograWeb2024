6e0dc42538d164a78f6d4e001ac1d5de867c3369
Jimena Azofeifa <107661297+JimenaAzofeifaPorras@users.noreply.github.com>
2024-02-03 15:30:58 -0600

    Clase#2

---
+++ Northwind/BackEnd/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using BackEnd.Models;
+using BackEnd.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService CategoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            CategoryService = categoryService;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<CategoryModel> Get()
+        {
+            return CategoryService.GetCategories();
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public CategoryModel Get(int id)
+        {
+            return CategoryService.GetById(id);
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
